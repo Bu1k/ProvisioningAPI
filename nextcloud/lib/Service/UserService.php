fa96db805663077cac93c3c11bc112fa918902f6
Eric Hocking <xaoseric@users.noreply.github.com>
2018-06-13 09:25:45 -0400

    add some common functions

--- nextcloud/lib/Service/UserService.php
+++ nextcloud/lib/Service/UserService.php
@@ -14,4 +14,30 @@ class UserService
   {
     $this->userManager->create($userId, $password);
   }
+  
+  public function delete($userId) 
+  {
+    return $this->userManager->get($userId)->delete();
+  }
+  
+  // recoveryPassword is used for the encryption app to recover the keys
+  public function setPassword($userId, $password, $recoveryPassword) 
+  {
+    return $this->userManager->get($userId)->setPassword($password, $recoveryPassword);
+  }
+
+  public function enable($userId) 
+  {
+    return $this->userManager->get($userId)->setEnabled(true); 
+  }
+  
+  public function disable($userId) 
+  {
+    return $this->userManager->get($userId)->setEnabled(false);
+  }
+
+  public function getHome($userId) 
+  {
+    return $this->userManager->get($userId)->getHome();
+  }
 }
