3827cd709dd3fe24d5b240ade7eaae96e9e1a75a
Eric Hocking <xaoseric@users.noreply.github.com>
2018-06-13 09:50:53 -0400

    update blesta module

--- blesta/Bu1kProvisioning.php
+++ blesta/Bu1kProvisioning.php
@@ -6,4 +6,20 @@ class Bu1kProvisioning extends Module {
         $this->loadConfig(dirname(__FILE__) . DS . "config.json");
     }
     
+    
+    public function getPackageFields($vars=null) {
+        // Create the labels
+        $fields = new ModuleFields();
+        $label_username = $fields->label("Username", "bu1k_nextcloud_username_id");
+        
+        // Create the fields
+        $user_field = $fields->fieldText("bu1k_nextcloud_username", "Username", array('id' => "bu1k_nextcloud_username_id"));
+        
+        // Attach the fields to the labels
+        $label_username->attach($user_field);
+ 
+        // Add the module fields
+        $fields->setField($label_username);
+        return $fields;   
+    }
 }
